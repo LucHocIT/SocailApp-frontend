837f5fcc7f619eb7b7bdffea4bc54fbddfd97ab1
MLuc24 <hiae2k4@gmail.com>
2025-05-21 13:09:40 +0700

    m

--- src/pages/RequestVerificationPage.jsx
+++ src/pages/RequestVerificationPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../context';
 import { toast } from 'react-toastify';
 
 const EmailSchema = Yup.object().shape({
